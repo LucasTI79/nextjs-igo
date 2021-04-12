af8bf92f573eaf4f3e5bcc31f2be330f5572b247
Lucas <lukasalves271@gmail.com>
2021-04-12 00:44:24 -0300

    Fix:add appid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,24 +6,21 @@ import '../styles/tailwind.css'
 import '../styles/globals.css'
 
 import Layout from '../components/Layout'
+import MessengerChat from '../Utils/MessengerChat'
 
 function MyApp({ Component, pageProps }) {
-  React.useEffect(() => {
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-  },[])
-  return (
+   return (
     <AnimateSharedLayout>
       <Layout>
         <Head>
           <title>IGO | Instituto Galindo de Odontologia</title>
         </Head>
         <Component {...pageProps} />
+
+        <MessengerChat
+          pageId="503018133206953"
+          ref="fb-msgr"
+        />
       </Layout>
     </AnimateSharedLayout>
   )
