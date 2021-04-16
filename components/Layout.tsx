a2a6ae542482b0b092a7933434d7f62acbe32a5a
Lucas <lukasalves271@gmail.com>
2021-04-15 21:25:05 -0300

    fix:add my plugin chat with message

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,11 @@
 import Header from './Header'
 import Footer from './Footer'
-import CostumerChat from './CostumerChat'
 
 const Layout: React.FC = ({ children }) => {
   return (
     <div className="flex flex-col justify-between mx-auto overflow-y-hidden max-w-5xl px-4 sm:px-6 md:px-8">
       <Header />
       {children}
-      <CostumerChat />
       <Footer />
     </div>
   )
