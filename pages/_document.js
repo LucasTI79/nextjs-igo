42f9c020988335cbd35b99820097f9604ac5d2a4
Lucas <lukasalves271@gmail.com>
2021-04-15 22:50:16 -0300

    feat:add Costumer chat

--- pages/_document.js
+++ pages/_document.js
@@ -31,14 +31,8 @@ class MyDocument extends Document {
                                 versão: 'v10.0'
                                 });
                             };
+                            `
 
-                            (função (d, s, id) {
-                            var js, fjs = d.getElementsByTagName (s) [0];
-                            if (d.getElementById (id)) retornar;
-                            js = d.createElement (s); js.id = id;
-                            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-                            fjs.parentNode.insertBefore (js, fjs);
-                           } (documento, 'script', 'facebook-jssdk')); `
                     }} />
 
 
